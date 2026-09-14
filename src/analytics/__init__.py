1f65bd617e73c8d9a7540482af0e756acda18004
Pramveer4949 <mrpramveer4949@gmail.com>
2026-09-14 21:44:08 +0530

    Create analytics package for water data
    Added AquaGuard Analytics Package with monitoring logic.

---
+++ src/analytics/__init__.py
@@ -0,0 +1,12 @@
+"""
+AquaGuard Analytics Package
+
+Contains analytical and monitoring logic for water
+consumption and water-quality data.
+"""
+
+from .analytics_engine import AnalyticsEngine
+
+__all__ = [
+    "AnalyticsEngine",
+]
